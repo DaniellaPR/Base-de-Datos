ebf09634e6cdb8771de3d539afce0ef9a6197f31
Daniela Pozo <159065844+DaniellaPR@users.noreply.github.com>
2025-12-01 09:33:06 -0500

    Update Clase13-ArquitecturaBD.sql

--- BD-2/Clase/Clase13-ArquitecturaBD.sql
+++ BD-2/Clase/Clase13-ArquitecturaBD.sql
@@ -12,7 +12,6 @@ SHOW con_name; --consulte en que pdb estamos trabajando con show name
 --si necseitamos cambiar de contenedor
 ALTER SESSION SET CONTAINER = CDB$ROOT;
 
-
 --Crear pdb desde seed usando el pdb matriz seed oslo plantilla de lectura para crear nuevos
 CREATE PLUGGABLE DATABASE DESAR  --ubicacion de desar abajo
 ADMIN USER udesar IDENTIFIED BY "lticPUCE24"
@@ -30,3 +29,11 @@ FILE_NAME_CONVERT = ('/u01/app/oracle/oradata/ORCL/pdbseed/', '/u01/app/oracle/o
 CREATE PLUGGABLE DATABASE COME
 ADMIN USER ucome IDENTIFIED BY "lticPUCE24"
 FILE_NAME_CONVERT = ('/u01/app/oracle/oradata/ORCL/pdbseed/', '/u01/app/oracle/oradata/ORCL/COME/');
+
+
+--1/12/2025
+--En máquina virtual:
+show pdbs --en SQL
+--Conectarse al cdb
+ALTER SESSION SET CONTAINER = CDB$ROOT;
+CREATE PLUGABLE DATABASE qa ADMIN USER adm_qa IDENTIFIED BY "lticPUCE24" FILE_NAME_CONVERT = ('/u01/app/oracle/oradata/ORCL/pdbseed/', '/u01/app/oracle/oradata/ORCL/QA/qa/');
